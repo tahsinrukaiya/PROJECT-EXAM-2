32063e42d79967e0152e1394a62e8de56c812460
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-10-18 14:46:37 +0200

    set up done

---
+++ HOLIDAZE/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar from './Components/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <h1>HOLIDAZE</h1>
+    </>
+  )
+}
+
+export default App
