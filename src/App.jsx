8be7c0fee1cf60d80c4b664d29cb61771582b81d
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-10-21 20:04:19 +0200

    working with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
+
 import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <>
       <NavBar />
-      <div><img src="../src/assets/hero-image.jpg" alt="lake side hotel" className="hero-image" />
-      </div>
+
     </>
   )
 }
