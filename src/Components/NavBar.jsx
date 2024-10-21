5d8b0f161d1b673d46619a60d52bb5c16446ab2b
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-10-21 19:32:50 +0200

    setting up files

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,11 @@
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NavBar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary nav_container">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src="./src/assets/logo.jpg" className="logo" /></a>
+                    <a className="navbar-brand mx-4" href="#"><img src="./src/assets/logo.jpg" className="logo" /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
