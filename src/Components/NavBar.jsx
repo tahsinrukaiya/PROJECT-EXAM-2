19d3886277caae0117631af3ca7b172b18cc4e75
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-10-22 20:21:52 +0200

    working on navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import Menubar from "../assets/bar.JPG";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -10,23 +11,23 @@ export default function NavBar() {
             <div className="container-fluid">
                 <a className="navbar-brand mx-4" href="#"><img src={Logo} className="logo" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"><img src={Menubar} className="bars" /></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-5 mb-2 mb-lg-0">
-                        <li className="nav-item me-4">
+                    <ul className="navbar-nav mx-5 mb-2 pb-4 mb-lg-0">
+                        <li className="nav-item me-5">
                             <NavLink to="/" className="links">Home</NavLink>
                         </li>
-                        <li className="nav-item me-4">
+                        <li className="nav-item me-5">
                             <NavLink to="/about_us" className="links">About Us</NavLink>
                         </li>
-                        <li className="nav-item me-4">
+                        <li className="nav-item me-5">
                             <NavLink to="/contact" className="links">Contact</NavLink>
                         </li>
-                        <li className="nav-item me-4">
+                        <li className="nav-item me-5">
                             <NavLink to="/login" className="links">Log In</NavLink>
                         </li>
-                        <li className="nav-item me-4">
+                        <li className="nav-item me-5">
                             <NavLink to="/logout" className="links">Log Out</NavLink>
                         </li>
                     </ul>
