13e34a5dc99e7d6e1807e88c2201c26482a79fa0
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-10-30 13:15:48 +0100

    seach form added

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -8,10 +8,21 @@ export default function HeroSection() {
                 <div className="overlay">
                     <h1 className="display-4 text-white slogan">Easy Booking, Memorable Stays</h1>
                     <p className="lead text-white">Raising comfort to the highest level</p>
-                    <a href="#" className="btn btn-primary btn-lg px-4 rounded-pill button-banner">Book venue here</a>
+                    <h3 className="lead-2 text-white">Find a venue that is perfect for your event and activities</h3>
+                    <div className="col-10 col-md-6 col-lg-6">
+                        <div className="input-group search-form">
+                            <input type="search" className="form-control rounded" placeholder="search" aria-label="Search" aria-describedby="search-addon" />
+                            <button type="button" className="search-btn  rounded-pill mx-2 px-3" data-mdb-ripple-init>Search</button>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
 
         </div>
     );
 }
+
+
+//    <a href="#" className="btn btn-primary btn-lg px-4 rounded-pill button-banner">Book venue here</a>
