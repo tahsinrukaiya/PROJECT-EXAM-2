71d593b35a4e2d11a6776025de1fb1a9deca9307
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-10-31 17:50:52 +0100

    small changes in config file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'],
+  build: {
+    rollupOptions: {
+      external: [
+        '/src/components/Pages/Venues/VenueCards'
+      ]
+    }
+  }
 })
 
 
