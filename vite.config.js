9b7cf508ae58b964e08c9d7c5563de033d7e2fb9
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-11-22 14:56:44 +0100

    config file set up

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
@@ -13,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@api': '/src/api',
+      '@api': path.resolve(__dirname, 'src/api'),
     },
   },
 });
