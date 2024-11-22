ba43853663ac8f20c06e40b24fdd5b1b8f11f549
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-11-22 17:07:56 +0100

    changed file path

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./Components/NavBar";
-import Home from "./Components/Pages/Home";
-import AllVenues from "./Components/Pages/Venues/AllVenues";
-import SingleVenue from "./Components/Pages/Venues/SingleVenue";
-import AboutUs from "./Components/Pages/AboutUs";
-import Contact from "./Components/Pages/Contact";
-import Profile from './Components/Pages/Profile';
-import CreateVenueForm from './Components/Pages/Venues/CreateVenueForm';
-import UpdateVenueForm from './Components/Pages/Venues/UpdateVenueForm';
-import LoginForm from './Components/Pages/Authentication/LoginForm';
-import LoginCustomer from './Components/Pages/Authentication/LoginCustomer';
-import LoginVenueManager from './Components/Pages/Authentication/LoginVenueManager';
-import RegisterType from "./Components/Pages/Authentication/RegisterType";
-import RegisterCustomer from "./Components/Pages/Authentication/RegisterCustomer";
-import RegisterVenueManager from "./Components/Pages/Authentication/RegisterVenueManger";
-import { AuthProvider } from "./Components/Pages/Authentication/AuthContext";
-import RouteNotFound from "./Components/RouteNotFound";
-import Footer from "./Components/Footer";
+import NavBar from "./components/NavBar";
+import Home from "./components/Pages/Home";
+import AllVenues from "./components/Pages/Venues/AllVenues";
+import SingleVenue from "./components/Pages/Venues/SingleVenue";
+import AboutUs from "./components/Pages/AboutUs";
+import Contact from "./components/Pages/Contact";
+import Profile from './components/Pages/Profile';
+import CreateVenueForm from './components/Pages/Venues/CreateVenueForm';
+import UpdateVenueForm from './components/Pages/Venues/UpdateVenueForm';
+import LoginForm from './components/Pages/Authentication/LoginForm';
+import LoginCustomer from './components/Pages/Authentication/LoginCustomer';
+import LoginVenueManager from './components/Pages/Authentication/LoginVenueManager';
+import RegisterType from "./components/Pages/Authentication/RegisterType";
+import RegisterCustomer from "./components/Pages/Authentication/RegisterCustomer";
+import RegisterVenueManager from "./components/Pages/Authentication/RegisterVenueManger";
+import { AuthProvider } from "./components/Pages/Authentication/AuthContext";
+import RouteNotFound from "./components/RouteNotFound";
+import Footer from "./components/Footer";
 
 
 function App() {
