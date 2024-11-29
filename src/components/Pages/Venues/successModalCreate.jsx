8dd4f3dc13297d0df31f4dbb29aeb7e3b0300b19
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-11-29 14:14:12 +0100

    small changes

--- src/components/Pages/Venues/successModalCreate.jsx
+++ src/components/Pages/Venues/successModalCreate.jsx
@@ -3,16 +3,23 @@ import { useNavigate } from "react-router-dom";
 
 export default function SuccessModalCreate({ show, onClose, message }) {
     const navigate = useNavigate();
+
     const handleClose = () => {
-        navigate = "/profile";
+        navigate("/profile");
+        onClose();
     };
 
     return (
         <Modal show={show} onHide={onClose}>
-            <Modal.Header closeButton onClick={handleClose}>
+            <Modal.Header closeButton>
                 <Modal.Title>Venue Creation Successful</Modal.Title>
             </Modal.Header>
             <Modal.Body>{message}</Modal.Body>
+            <Modal.Footer>
+                <Button className="close-btn rounded-pill" variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
         </Modal>
     );
 }
