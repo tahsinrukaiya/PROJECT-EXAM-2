b6b60dc2058df189da15195c298e881b22b3b2a4
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-11-29 14:04:14 +0100

    netlify fix

--- src/components/Pages/Venues/successModalCreate.jsx
+++ src/components/Pages/Venues/successModalCreate.jsx
@@ -1,9 +1,15 @@
 import { Modal, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function SuccessModalCreate({ show, onClose, message }) {
+    const navigate = useNavigate();
+    const handleClose = () => {
+        navigate = "/profile";
+    };
+
     return (
         <Modal show={show} onHide={onClose}>
-            <Modal.Header closeButton>
+            <Modal.Header closeButton onClick={handleClose}>
                 <Modal.Title>Venue Creation Successful</Modal.Title>
             </Modal.Header>
             <Modal.Body>{message}</Modal.Body>
